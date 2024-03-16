fe005e048b4bc4efcd3358df11df8e5571490292
Nouman-blip <attaussmalikki@gmail.com>
2024-03-16 20:31:45 +0500

    make some chnages

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,15 +1,16 @@
-import { Module } from "@nestjs/common";
+import { Module ,forwardRef} from "@nestjs/common";
 import { UserService } from "./user.service";
 import { MongooseModule } from "@nestjs/mongoose";
 import { UsersController } from "./user.controller";
 import { UserSchema } from "src/schemas/user/user.schema";
+import { CourseModule } from "src/course/course.module";
 
 
 @Module({
-    imports:[MongooseModule.forFeature([{name:'User', schema:UserSchema}])],
+    imports:[MongooseModule.forFeature([{name:'User', schema:UserSchema}]),forwardRef(() => CourseModule)],
     controllers:[UsersController],
     providers:[UserService],
-    exports:[UserModule,MongooseModule]
+    exports:[UserModule,MongooseModule,UserService]
 })
 export class UserModule {
 
